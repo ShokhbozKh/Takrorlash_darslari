5cd619946bd05ea1f2c0f10a23a2288743fb8fb4
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-26 01:04:17 +0500

    Add files via upload

---
+++ #2haqiqiy sonlar.js
@@ -0,0 +1,48 @@
+//#1
+// let n=768;
+// console.log(Math.trunc(n/100));
+
+//#2
+// let n=34;
+// end=n%10;
+// start=Math.trunc(n/10);
+// summ=(end+start);
+// console.log('boshi:',start,'\noxiri:',end,'\n summa:',summ);
+
+//#3
+// let a=2,b=3,c=4;
+// p=(a+b+c)/2;
+// s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+// console.log('p=',p,'\ns=',s.toFixed(2));
+
+//#4
+// let c=5,a=3;
+// b=(c**2-a**2)**0.5;
+// r=(a+b+c)/a*b ;
+// console.log('b=',b,'\nc=',c);
+
+
+//#5
+// let a=4,d=6,n=8;
+// s=(2*a+d*(n-1))*n/2;
+// console.log('s=',s);
+
+//#6
+// let x1=1,x2=2,y1=3,y2=4;
+// d=((x1-x2)**2 + (y1-y2)**2)**0.5;
+// console.log('d=',d.toFixed(4));//4 xona yaxlitlash
+
+//#7
+// let a=9,b=2;
+// console.log(Math.trunc(a/b));
+
+//#8
+// let a=8,b=3;
+// console.log((a/b).toFixed(4));
+
+//#9
+// let a=1,b=2;
+// let c=a;
+// a==b;
+// b==c;
+// console.log('a=',b,'\nb=',a);
