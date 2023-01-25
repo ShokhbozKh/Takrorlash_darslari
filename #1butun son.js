b9d8c5ff252560b3d3aebe1cb3fe4b8a5c8ddeac
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-25 17:07:26 +0500

    Add files via upload

---
+++ #1butun son.js
@@ -0,0 +1,47 @@
+//#1
+// let a=4;
+// s=6*(a**2);
+// v=a**3;
+// console.log("s=",s,'\n v=',v);
+
+//#2
+// let a=4,b=3;
+// S=(a+b)/2;
+// c=(a**2+b**2)**0.5;
+// console.log(S,' ',c);
+
+//#3
+// let a=4;b=6;
+// geo=(a*b)**0.5; alg=(a+b)/2;
+// console.log(geo,' ',alg);
+
+//#4
+// let h=24,g=10;
+// t=(2*h/g)**0.5;
+// console.log(t);
+
+//#5
+// let a=4;
+// s=((3)**0.5 /4) *a*a;
+// console.log(s);
+
+//#6
+// let l=12;
+// const pi=3.14,g=10;
+// t=2*pi*(l/g);
+// console.log(t);
+
+//#7
+// let t1=1,t2=100,v1=24,v2=40;
+// t=((v1+v2)*t2+v2*t1)/(2*v1+v2);
+// console.log(t);
+
+//#8
+// let r1=1,r2=2,r3=3;
+// r=(r1*r2*r3)/(r1*r2+r1*r3+r2*r3);
+// console.log(r);
+
+//#9
+// let x=0.4,y=-9.5;
+// console.log((Math.abs(x)-Math.abs(y))) / (1+Math.abs(x*y));
+
