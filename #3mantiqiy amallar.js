14303892a7ae251fe0cd306da89b52b8e5378010
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-27 17:12:38 +0500

    Add files via upload

---
+++ #3mantiqiy amallar.js
@@ -0,0 +1,33 @@
+//#1
+// let a=true,b=false,d=false,c=true;
+// console.log((a&&b)||(c&&d)&&(a||b));
+
+//#2
+// let a=9,b=-7;
+// console.log((a>0 &&b<0)||(a<0 && b>0));
+
+//#3
+// let a=2,b=-2;
+// n=(a>0&&b<0)||(a<0&&b>0);
+// console.log(n);
+
+//#4
+// let n=4;
+// console.log(n%2==0)
+
+//#5
+// let a=3,b=5;
+// n=(a%2!=0 && b%2!=0);
+// console.log(n);
+
+//#6
+// let a=5,b=8;
+// console.log((a%2!=0 && b%2==0)||(a%2==0&&b%2!=0));
+
+//#7
+// let a=3,b=4,c=5;
+// console.log(c**2==((a**2+b**2)));
+
+//#8
+// let a=2,b=4,c=5;
+// console.log((a+b>c&&a+c>b&&c+b>a));
