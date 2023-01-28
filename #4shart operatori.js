092491dd9da4f7e84c0544ea66f3165ce9e6913b
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-28 23:06:24 +0500

    Add files via upload

---
+++ #4shart operatori.js
@@ -0,0 +1,157 @@
+//#1////////////////////////////////////////
+// let x=2,y=4;
+// //1.1kattasi
+// if(x>y){
+//     console.log(x);
+// }else{
+//     console.log(y);
+// }
+
+// //1.2kichigi//////////////////////////
+// if(x<y){
+//     console.log(x);
+// }else{
+//     console.log(y);
+// }
+
+//#2//////////////////////////////////////
+// let x=1,y=2,z=3;
+// n='';
+// //2.1////////////////////
+// if(x>y&&x>z){
+//     n=x;
+// }else if(y>x&&y>z){
+//     n=y;
+// }else{
+//     n=z;
+// }console.log(n);
+//2.2////////////////////////
+// (x<y&&x<z)?n=x:(y<x&&y<z)?n=y:n=z;
+// console.log(n);
+
+//#3/////////////////////////////////////////
+// let x=2,y=4,z=6;
+// n='';
+// //3.1
+// (x+y+z)>(x*y*z)?n='yigindi':n='kupaytma';
+// console.log(n);
+
+// //3.2
+// (x+y+z/2)<x*y*z?n=(x*y*z):n=(x+y+z/2);
+// console.log(n**2 +1);
+
+//#4
+// let a=1,b=2,c=3;
+// n='';
+// (a>b&&a>c)?n='bajarildi':n='bajarilmadi';
+// console.log(n);
+
+//#5
+// let a=3,b=2,c=1;
+// if(a>b&& a>c){
+//     console.log(a*2,' ',b*2,' ',c*2);
+// }else{
+//     console.log(Math.abs(a),' ',Math.abs(b),' ',Math.abs(c));
+// }
+
+//#6
+// let x=3,y=4;
+// if(x>y){
+//     z=x-y;
+// }else{
+//     z=y-x+1;
+// }console.log(z);
+
+//#7
+// let a=3,b=5;
+// if(a>b){
+//     console.log(a);
+// }else{
+//     console.log(a,b);
+// }
+
+// #8
+// let a=4,b=7;
+// if(a<=b){
+//     a=0;
+//     console.log(a);
+// }else{
+//     console.log(a,b);
+// }
+
+//#9
+// let a=1,b=2,c=3;
+// if(a>1&&a<3){
+//     console.log(a);
+// }else if(b>1 && b<3){
+//     console.log(b);
+// }else if(c>1&&c<3){
+//     console.log(c)
+// }
+
+//#10
+// let x=3,y=7;
+// if(x<y){
+//     n=(Math.trunc(x/2)*y)*2;
+// }else{
+//     n=(Math.trunc(y/2)*x)*2;
+// }console.log(n);
+
+//#11
+// let a=2,b=-4,c=6;
+// n='';
+// if(a>0){
+//     console.log(a*a);
+// }
+// if(b>0){
+//     console.log(b*b);
+// }
+// if(c>0){
+//     console.log(c*c);
+// }
+// else{
+//     console.log('hammasi manfiy');
+// }
+
+//#12
+// let a=1,b=3,c=5,d=6;
+// if(d>=c && d>=b &&d>=a){
+//     a=d,b=d,c=d,d=d;
+//     console.log(a,b,c,d);
+// }else if(a>b && a>c &&a>d){
+//     console.log(a,b,c,d);
+// }else{
+//     console.log(a*a,b*b,c*c,d*d);
+// }
+
+//#13
+// let a=2,b=5,c=2;
+// d=Math.sqrt(b*b-4*a*c);;
+// if(d>0){
+
+//     x1=(-b+d)/2*a;
+//     x2=(-b-d)/2*a;
+//     console.log(x1,x2);
+// }else{
+//     console.log("d manfiy");
+// }
+
+//#14
+
+////////Y//////////
+// X             //
+//               //
+////////////////////
+
+// let x=2,y=5;
+// let a=2,b=5,c=3;
+// if(x==a && y==b){
+//     console.log('sig\'adi');
+// }else if(x==b && y==c){
+//     console.log('sig\'adi');
+// }else if(x==c && y==a){
+//     console.log('sig\'adi');
+// }else{
+//     console.log('sig\'maydi');
+// }
+
