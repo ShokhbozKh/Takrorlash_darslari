1fb43414dcd8a53139ad130f1448a97506dde310
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-01-31 22:01:45 +0500

    Add files via upload

---
+++ #5tanlash operatorlari1.js
@@ -0,0 +1,105 @@
+//#1
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("Yanvar"); break;
+//     case 2:
+//         console.log("fevral");break;
+//     case 3:
+//         console.log('mart');break;
+//     case 4:
+//         console.log('aprel');break;
+//     case 5:
+//         console.log('may');break;
+//     case 6:
+//         console.log('iyun');break;
+//         /////////////....//
+//     default:
+//         console.log('bunday oy yuq');
+// }
+
+//#2
+// let month=6;
+// switch(month){
+//     case 1:
+//     case 12:
+//     case 2:
+//         console.log('qish');break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('bahor');break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('yoz');break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('kuz');break;
+//         default:
+//             console.log("xotooo");
+//}
+
+//#3
+// let fasl=4;
+// switch(fasl){
+//     case 1:
+//         conaole.log("Dekabr\nYAnvar\nFevral");break;
+//     case 2:
+//         console.log('Mart\nAprel\nMay');break;
+//     case 3:
+//         console.log('Iyun\n Iyul \nAvgust');
+//         break;
+//     case 4:
+//         console.log('Sentyabr\n Oktyabr\n Noyabr');break;
+// }
+
+//#4
+// let number=79;
+// bir=number%10;
+// un=Math.trunc(number/10);
+// n='';
+
+// switch(un){
+//     case 1:
+//         n+='un '; break;
+//     case 2:
+//         n+='yigirma '; break;
+//     case 3:
+//         n+='uttiz '; break;
+//     case 4:
+//         n+='qirq '; break;
+//     case 5:
+//         n+='ellik '; break;
+//     case 6:
+//         n+='oltmish '; break;
+//     case 7:
+//         n+='yetmish '; break;
+//     case 8:
+//         n+='sakson '; break;
+//     case 9:
+//         n+='to\'qson '; break;
+
+// }
+// switch(bir){
+//     case 1:
+//         n+='bir';break;
+//     case 2:
+//         n+='ikki';break;
+//     case 3:
+//         n+='uch';break;
+//     case 4:
+//         n+='turt';break;
+//     case 5:
+//         n+='besh';break;
+//     case 6:
+//         n+='olti';break;
+//     case 7:
+//         n+='yetti';break;
+//     case 8:
+//         n+='sakkiz';break;
+//     case 9:
+//         n+='tuqqiz';break;
+// }
+// console.log(n);
